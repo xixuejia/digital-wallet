40fd02c4809debb8743016a824b50f262c5d71e9
Xi Xue Jia <xixuejia@gmail.com>
2020-07-17 09:11:57 +0800

    fix security identity test

--- fabric/gosdk/utilities/secure_identity_test.go
+++ fabric/gosdk/utilities/secure_identity_test.go
@@ -6,19 +6,19 @@ import (
 )
 
 func TestSecureSigningIdentity(t *testing.T) {
-	signingId, err := NewSecureIdentity(os.ExpandEnv("$GOPATH/src/github.com/xixuejia/digital-wallet/fabric/gosdk/"+
-		"fixtures/sdk-crypto/org1.example.com/users/Admin@org1.example.com/msp"), "org1")
+	signingID, err := NewSecureIdentity(os.ExpandEnv("$GOPATH/src/github.com/xixuejia/digital-wallet/fabric/gosdk/"+
+		"fixtures/sdk-crypto/org1.example.com/users/Admin@org1.example.com/msp"), "org1", 1)
 	if err != nil {
 		t.Errorf("Error creating signing id: %s", err)
 		t.FailNow()
 	}
 	msg := "hello world"
-	signature, err := signingId.Sign([]byte(msg))
+	signature, err := signingID.Sign([]byte(msg))
 	if err != nil {
 		t.Errorf("Error signing msg: %s", err)
 		t.FailNow()
 	}
-	if err := signingId.Verify([]byte(msg), signature); err != nil {
+	if err := signingID.Verify([]byte(msg), signature); err != nil {
 		t.Errorf("Error verifying signature: %s", err)
 		t.FailNow()
 	}
